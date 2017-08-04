d86cf398a7d5569c46b9b5749f0a66f291712921
poiuj <poiuj@users.noreply.github.com>
2017-08-04 18:43:58 +0300

    Encode handshake message from user to server

--- src/encode.lisp
+++ src/encode.lisp
@@ -1,5 +1,6 @@
 (uiop:define-package :src/encode
-    (:use :common-lisp))
+    (:use :common-lisp :src/game-protocol)
+  (:export #:encode-me))
 
 (declaim (optimize (debug 3) (safety 3)))
 
@@ -14,3 +15,10 @@
           (yason:encode-object-element "source" (claim-source obj))
           (yason:encode-object-element "target" (claim-target obj)))))))
 
+(defun encode-me (name)
+  (let ((result-str
+         (with-output-to-string (stream)
+           (yason:with-output (stream)
+             (yason:with-object ()
+               (yason:encode-object-element "me" name))))))
+    (format nil "~a:~a" (length result-str) result-str)))
